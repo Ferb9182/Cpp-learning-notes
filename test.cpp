cd10934d9e8869b7a1bc67fa8514d60c58d46331
Ferb9182 <37694534+Ferb9182@users.noreply.github.com>
2018-06-07 02:02:22 +0200

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,18 @@
+/*test1 calculator*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter two num:" << std::cout;
+	int v1, v2;
+	std::cin >> v1 >> v2;
+	std::cout << "The nsum of " << v1 << "and" << v2
+			  << " is " << v1 + v2 << std::end1;
+			  
+	return 0;
+	
+
+
+
+}
